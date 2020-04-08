1cc364e942fff45763e9279a960ceee44e2ff576
Karl Semich <0xloem@gmail.com>
2020-04-08 17:29:35 +0000

    moved less relevent code towards bottom

--- example.cpp
+++ example.cpp
@@ -2,17 +2,7 @@
 
 #include <iostream>
 
-void dump_metadata(sia::skynet::response::subfile & metadata, std::string indent = "")
-{
-	std::cout << indent << "contenttype: " << metadata.contenttype << std::endl;
-	std::cout << indent << "filename: " << metadata.filename << std::endl;
-	std::cout << indent << "len: " << metadata.len << std::endl;
-	auto biggerindent = indent + "  ";
-	for (auto & file : metadata.subfiles) {
-		std::cout << indent << file.first << std::endl;
-		dump_metadata(file.second, biggerindent);
-	}
-}
+void dump_metadata_hierarchy(sia::skynet::response::subfile & metadata, std::string indent = "");
 
 int main()
 {
@@ -30,8 +20,21 @@ int main()
 
 	std::cout << "skylink: " << response.skylink << std::endl;
 	std::cout << "filename: " << response.filename << std::endl;
-	dump_metadata(response.metadata);
 	std::cout << "data: " << std::string(response.data.begin(), response.data.end()) << std::endl;
+	std::cout << "metadata: " << std::endl;
+	dump_metadata_hierarchy(response.metadata, "  ");
 
 	return 0;
+}
+
+void dump_metadata_hierarchy(sia::skynet::response::subfile & metadata, std::string indent)
+{
+	std::cout << indent << "contenttype: " << metadata.contenttype << std::endl;
+	std::cout << indent << "filename: " << metadata.filename << std::endl;
+	std::cout << indent << "len: " << metadata.len << std::endl;
+	auto biggerindent = indent + "  ";
+	for (auto & file : metadata.subfiles) {
+		std::cout << indent << file.first << std::endl;
+		dump_metadata_hierarchy(file.second, biggerindent);
+	}
 }
