72dcffdccda6bda48d9c8c1ae3dad7dc97b1754e
Karl Semich <0xloem@gmail.com>
2020-05-12 18:56:53 -0400

    skystream: fix bug with json layout

--- skystream.cpp
+++ skystream.cpp
@@ -85,10 +85,12 @@ int main(int argc, char **argv)
 	sia::skynet::upload_data content("content", "", "application/octet-stream");
 	sia::skynet::upload_data metadata_upload("metadata.json", "", "application/json");
 
-	content.data.reserve(1024 * 1024);
+	content.data.reserve(1024 * 1024 * 16);
 	content.data.resize(content.data.capacity());
 
-	nlohmann::json metadata_json;
+	nlohmann::json metadata_json{
+		{"sia-skynet-stream", "1.0.7"}
+	};
 
 	std::vector<nlohmann::json> lookup_nodes; // roots and spans to use to seek stream history
 
@@ -103,34 +105,39 @@ int main(int argc, char **argv)
 		//content.filename = content_sha3_512;
 		content.data.resize(size);
 
-		metadata_json = { // make a new object so we don't rewrite old lookup nodes stored away
-			{"sia-skynet-stream", "1.0.4"},
-			{"content", {
-				{"spans", {
-					{"time", {{"start", start_time}, {"end", end_time}, {"length", end_time - start_time}}},
-					{"bytes", {{"start", offset}, {"end", offset + size}, {"length", size}}},
-					{"index", {{"start", index},{"end", index}, {"length", 0}}}
-				}},
-				{"identifiers", content_identifiers}
+		metadata_json["content"] = {
+			{"spans", {
+				{"time", {{"start", start_time}, {"end", end_time}, {"length", end_time - start_time}}},
+				{"bytes", {{"start", offset}, {"end", offset + size}, {"length", size}}},
+				{"index", {{"start", index},{"end", index}, {"length", 0}}}
 			}},
-			{"lookup", lookup_nodes}
+			{"identifiers", content_identifiers}
 		};
+		metadata_json["lookup"] = lookup_nodes;
 		std::string metadata_string = metadata_json.dump();
 		std::cout << metadata_string << std::endl;
 		metadata_upload.data = std::vector<uint8_t>(metadata_string.begin(), metadata_string.end());
 		auto metadata_identifiers = cryptography.digests({&metadata_upload.data});
-		std::string skylink = portal.upload(metadata_identifiers["sha3_512"], {metadata_upload, content});
+		std::string skylink;
+		while (true) {
+			try {
+		       		skylink = portal.upload(metadata_identifiers["sha3_512"], {metadata_upload, content});
+				break;
+			} catch(std::runtime_error const & e) {
+				std::cerr << e.what() << std::endl;
+				continue;
+			}
+		}
 		metadata_identifiers["skylink"] = skylink + "/" + metadata_upload.filename;
 
-		metadata_json["content"]["identifiers"] = metadata_identifiers;
 		size_t depth = 0;
-		metadata_json.erase("lookup");
-		metadata_json.erase("sia-skynet-stream");
+		auto new_lookup_node = metadata_json["content"];
+		new_lookup_node["identifiers"] = metadata_identifiers;
 
 		while (lookup_nodes.size() && lookup_nodes.back()["depth"] == depth) {
 			auto & back = lookup_nodes.back();
 			auto & back_spans = back["spans"];
-			for (auto & span : metadata_json["spans"].items()) {
+			for (auto & span : new_lookup_node["spans"].items()) {
 				auto start = back_spans[span.key()]["start"];
 				span.value()["start"] = start;
 				span.value()["length"] = (double)span.value()["end"] - (double)start;
@@ -138,16 +145,18 @@ int main(int argc, char **argv)
 			lookup_nodes.pop_back();
 			++ depth;
 		}
-		metadata_json["depth"] = depth;
-		lookup_nodes.emplace_back(metadata_json);
+		new_lookup_node["depth"] = depth;
+		lookup_nodes.emplace_back(new_lookup_node);
 
 		++ index;
 		offset += size;
 
 		start_time = end_time;
+		/*
 		if (content.data.size() == content.data.capacity()) {
 			content.data.reserve(content.data.capacity()*2);
 		}
+		*/
 		content.data.resize(content.data.capacity());
 	}
 	std::cout << lookup_nodes.back()["content"]["identifiers"].dump(2) << std::endl;
