2bc3c0a8d968fc1ced4d7ba5bd3c82144925a0a2
Karl Semich <0xloem@gmail.com>
2020-05-12 21:27:57 -0400

    move stream tools into folder and add blake2b hash

--- tools/stream-up.cpp
+++ tools/stream-up.cpp
@@ -9,64 +9,8 @@
 
 #include <siaskynet.hpp>
 
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
+#include "crypto.hpp"
 
-class crypto
-{
-public:
-	crypto()
-	{
-		ERR_load_crypto_strings();
-		OpenSSL_add_all_algorithms();
-		mdctx = EVP_MD_CTX_create();
-	}
-	~crypto()
-	{
-		EVP_MD_CTX_destroy(mdctx);
-		EVP_cleanup();
-		CRYPTO_cleanup_all_ex_data();
-		ERR_free_strings();
-	}
-	std::string digest(std::initializer_list<std::vector<uint8_t> const *> data, decltype(EVP_sha3_512()) algorithm)
-	{
-		static thread_local std::string result;
-		static thread_local std::vector<uint8_t> bytes;
-		bytes.resize(EVP_MAX_MD_SIZE);
-
-		EVP_DigestInit_ex(mdctx, algorithm, NULL);
-
-		for (auto & chunk : data) {
-			EVP_DigestUpdate(mdctx, chunk->data(), chunk->size());
-		}
-
-		unsigned int size;
-		EVP_DigestFinal_ex(mdctx, bytes.data(), &size);
-		bytes.resize(size);
-
-		result.resize(size * 2);
-
-		static char hex[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-
-		for (int i = 0; i < size; ++ i) {
-			int j = i << 1;
-			result[j] = hex[bytes[i] >> 4];
-			result[j+1] = hex[bytes[i] & 0xf];
-		}
-		return result;
-	}
-	nlohmann::json digests(std::initializer_list<std::vector<uint8_t> const *> data)
-	{
-		return {
-			{"sha3_512", digest(data, EVP_sha3_512())},
-			{"sha512_256", digest(data, EVP_sha512_256())}
-		};
-	}
-
-private:
-	EVP_MD_CTX * mdctx;
-};
 double time()
 {
 	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count() / double(1000000);
